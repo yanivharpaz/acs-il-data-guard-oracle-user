722bd5ff59407a52b56a3daa0d9c1552065c7993
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-10-25 19:00:18 +0300

    initial

---
+++ scripts/112_open_port.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: sudo $0 [ PORT ] "
+    exit 1
+fi
+
+PORT_TO_OPEN=$1
+
+echo "Type 'sudo iptables -L' to see the list of ports that are open"
+echo "Opening port: $PORT_TO_OPEN"
+
+echo sudo /usr/sbin/iptables -I INPUT -p tcp --dport $PORT_TO_OPEN -j ACCEPT -m comment --comment "Allow Oracle RDBMS"
+sudo /usr/sbin/iptables -I INPUT -p tcp --dport $PORT_TO_OPEN -j ACCEPT -m comment --comment "Allow Oracle RDBMS"
+
+exit 0
